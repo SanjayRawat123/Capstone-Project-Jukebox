84cd8cdfc7b4160ef98f9a63eb7b7a3cd22083e9
Sanjay <rawatsanjay2799@gmail.com>
2022-09-21 09:43:45 +0530

    created setter and getter method for  field

--- src/main/java/com/niit/jdp/model/Playlist.java
+++ src/main/java/com/niit/jdp/model/Playlist.java
@@ -14,4 +14,50 @@ public class Playlist {
     private String albumName;
     private String genre;
     private String songPath;
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public Playlist setSongId(int songId) {
+        this.songId = songId;
+        return this;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public Playlist setSongName(String songName) {
+        this.songName = songName;
+        return this;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public Playlist setAlbumName(String albumName) {
+        this.albumName = albumName;
+        return this;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public Playlist setGenre(String genre) {
+        this.genre = genre;
+        return this;
+    }
+
+    public String getSongPath() {
+        return songPath;
+    }
+
+    public Playlist setSongPath(String songPath) {
+        this.songPath = songPath;
+        return this;
+    }
+
 }
