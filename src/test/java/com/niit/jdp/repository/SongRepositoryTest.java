172ed95eb4619711f0ff0b23ce80f465aa2f23d5
Sanjay <rawatsanjay2799@gmail.com>
2022-09-26 10:12:11 +0530

    getAll This function is used to read all the songs from the database and return them as a list of songs

---
+++ src/test/java/com/niit/jdp/repository/SongRepositoryTest.java
@@ -0,0 +1,60 @@
+/*
+ * Author Name:
+ * Date: 9/26/2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+
+
+package com.niit.jdp.repository;
+
+
+import com.niit.jdp.model.Song;
+import com.niit.jdp.service.DatabaseService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class SongRepositoryTest {
+    SongRepository songRepository = null;
+    Song song;
+    DatabaseService databaseService = new DatabaseService();
+
+
+    @BeforeEach
+    void setUp() {
+        songRepository = new SongRepository();
+        song = new Song();
+        databaseService = new DatabaseService();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        songRepository = null;
+        song = null;
+    }
+
+
+    @Test
+    void getSongByIdSuccess() throws SQLException, ClassNotFoundException {
+        databaseService.connect();
+        Connection connection = databaseService.getConnection();
+        Song output = songRepository.getById(connection, song.getSongId());
+        Song expectedOutput = songRepository.getById(connection, output.getSongId());
+        Assertions.assertEquals(expectedOutput, output);
+    }
+
+    @Test
+    void getSongByIdFailure() throws SQLException, ClassNotFoundException {
+        databaseService.connect();
+        Connection connection = databaseService.getConnection();
+        Song expectedOutput = songRepository.getById(connection, song.getSongId());
+        boolean output = songRepository.deleteById(connection, song.getSongId());
+        Assertions.assertNotEquals(expectedOutput, output);
+    }
+
+}
