49a3c02551cfcb8f4e2ca02cb55421c1c956fc8a
Sanjay <rawatsanjay2799@gmail.com>
2022-09-26 10:30:02 +0530

    created getSortedNameByOrder method in SongService class

---
+++ src/main/java/com/niit/jdp/service/SongService.java
@@ -0,0 +1,30 @@
+/*
+ * Author Name:
+ * Date: 9/25/2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+
+
+package com.niit.jdp.service;
+
+import com.niit.jdp.model.Song;
+
+import java.util.Comparator;
+import java.util.List;
+
+
+public class SongService {
+    public List<Song> getSortedNameByOrder(List<Song> songName) {
+
+        Comparator<Song> nameComparator = ((o1, o2) -> String.CASE_INSENSITIVE_ORDER.compare(o1.getSongName(), o2.getSongName()));
+        songName.sort(nameComparator);
+        for (Song songs : songName) {
+            songs.getSongName();
+        }
+        return songName;
+    }
+
+}
+
+
+
