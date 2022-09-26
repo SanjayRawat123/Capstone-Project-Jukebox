72035b3f03902b5291c070f8b811a9ef7fec4ec1
Sanjay <rawatsanjay2799@gmail.com>
2022-09-26 10:38:31 +0530

    created DatabaseServiceTest.java

---
+++ src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
@@ -0,0 +1,47 @@
+/*
+ * Author Name:
+ * Date: 9/26/2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+
+
+package com.niit.jdp.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DatabaseServiceTest {
+
+    DatabaseService databaseService;
+
+    @BeforeEach
+    void setUp() {
+        databaseService = new DatabaseService();
+    }
+
+    @AfterEach
+    void tearDown() {
+        databaseService = null;
+    }
+
+    @Test
+    void getConnectionSuccess() throws SQLException, ClassNotFoundException {
+        boolean actualOutput = databaseService.connect();
+        boolean expectedOutput = databaseService.connect();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void getConnectionFailure() throws SQLException, ClassNotFoundException {
+        Connection actualOutput = databaseService.getConnection();
+        boolean expectedOutput = databaseService.connect();
+        Assertions.assertNotEquals(expectedOutput, actualOutput);
+    }
+
+
+}
